b93bd16d1d2a9348e377a6fb87b92cb81b7aa658
nvest <nicole.marie.vest@gmail.com>
2025-01-01 17:17:10 -0600

    hide and show job role text box

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-const nameInput = document.getElementById("name");
+const nameInput = document.querySelector("#name");
+const jobRoleSelect = document.querySelector("#title");
+const otherJobRoleInput = document.querySelector("#other-job-role")
+
 
 // Focus on the first form field
 nameInput.focus();
 
+// Only show text box when other job role is selected
+otherJobRoleInput.hidden = true;
+jobRoleSelect.addEventListener("change", () => {
+                    
+    if(jobRoleSelect.value == "other") {
+        otherJobRoleInput.hidden = false;
+    } else {
+        otherJobRoleInput.hidden = true;
+    }
+
+
+});
+  
