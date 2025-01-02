2e0a5acdf2ec17dea5d2c52994c181c8dc78e0ed
nvest <nicole.marie.vest@gmail.com>
2025-01-01 18:02:53 -0600

    show only relevant colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const nameInput = document.querySelector("#name");
 const jobRoleSelect = document.querySelector("#title");
-const otherJobRoleInput = document.querySelector("#other-job-role")
+const otherJobRoleInput = document.querySelector("#other-job-role");
+const designSelect = document.querySelector("#design");
+const colorSelect = document.querySelector("#color");
 
 
 // Focus on the first form field
@@ -15,7 +17,27 @@ jobRoleSelect.addEventListener("change", () => {
     } else {
         otherJobRoleInput.hidden = true;
     }
-
-
 });
-  
+
+
+// Show color options only for their available designs
+colorSelect.disabled = true;  
+designSelect.addEventListener("change", () => {
+
+    colorSelect.disabled = false;
+
+    // Loop through color options and show the ones that are available for the selected design.
+    // Select the current color                              
+    for (let i = 0; i < colorSelect.children.length; i++) {
+        const colorValue = colorSelect.children[i];
+        const colorDesign = colorValue.getAttribute("data-theme");
+      
+        if(designSelect.value == colorDesign) {
+            colorValue.hidden = false;
+            colorValue.setAttribute("selected", true);
+        } else {
+            colorValue.hidden = true;
+            colorValue.removeAttribute("selected", true)
+        }
+    }
+});
